0c8e849042dba62f41c4a7ee4cf828558be1dfff
John Vincent <corcegajovic@gmail.com>
2021-02-07 17:47:44 +0800

    results: Initial work for the results page.

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GameDataService } from '../../services/game-data.service';
 
 @Component({
   selector: 'app-results',
@@ -8,12 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultsPage implements OnInit {
   gameResult: any;
-  constructor(private route: ActivatedRoute) { }
+  gameStars: any;
+  constructor(private route: ActivatedRoute, private scoreData: GameDataService) { }
 
   ngOnInit() {
     if (this.route.snapshot.data['currentGameScore']) {
+      this.gameStars = this.scoreData.getGameData('currentGameStars');
       this.gameResult = this.route.snapshot.data['currentGameScore'];
       console.log('Inf: Player score for game is ' + this.route.snapshot.data['currentGameScore']);
+      console.log('Inf: Player star for game is ' + this.gameStars);
     }
   }
 
