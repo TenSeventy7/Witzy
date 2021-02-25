2964d460ec7b544a3288fa8757863ceaada59ab0
John Vincent <corcegajovic@gmail.com>
2021-02-25 20:31:59 +0800

    pages/Splash: Reflect changes to SplashComponent
    See c0d75c187eea713438311f354992cced1ad28497.

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-splash',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashPage implements OnInit {
 
-  constructor() { }
+  splashProgress: number;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.splashProgress = 0.5;
+
+    this.http.get('/assets/bodymovin/space-bg.json', {responseType: 'json'}).subscribe(
+      (data) => {
+        this.http.get('/assets/bodymovin/spaceman.json', {responseType: 'json'}).subscribe(
+          (bg) => {
+          console.log(bg);
+          console.log(data);
+    
+          setTimeout(()=> {
+            this.splashProgress = 0.8;
+          this.router.navigate(['/home']);
+          }, 3000);
+        });
+    });
   }
 
 }
