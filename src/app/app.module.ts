0e18a8df8857364be051f39d1a5fa5390b0c915b
John Vincent <corcegajovic@gmail.com>
2021-02-25 16:17:56 +0800

    package: Fix compilation on production builds
    - Disable "Ivy" compiler
    - Temporarily disable iOS transitions as this aborts compile. New transition animations are currently in progress.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { iosTransitionAnimation } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot({
-    navAnimation: iosTransitionAnimation,
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot({
     mode: 'md'
  }), AppRoutingModule],
