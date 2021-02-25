c0d75c187eea713438311f354992cced1ad28497
John Vincent <corcegajovic@gmail.com>
2021-02-25 20:31:00 +0800

    components/Splash: Make the splash component extensible
    We will use the splash component in modals where loading is required. Making it flexible will allow us to programmatically change progress and stuff, plus put it into modals.

--- src/app/components/splash/splash.component.ts
+++ src/app/components/splash/splash.component.ts
@@ -1,45 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-loader',
+  selector: 'app-splash-screen',
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss'],
 })
 
 export class SplashComponent implements OnInit {
-  public progress: number = 0.0
-  public progressDots: number = 0.0
-  loadTime = 0
+  @Input() progress: number;
 
-  constructor(private router: Router) { }
-
-  public addProgress(newProgress: number, progessTime: number, newActivity: string) {
-    setTimeout(()=> {
-      this.progress = newProgress;
-      this.progressDots = newProgress;
-
-      if (newProgress == 1) {
-        setTimeout(()=> {
-          this.router.navigate([newActivity]);
-        }, progessTime);
-      }
-      
-    }, progessTime);
-  };
-
-  public goToActivity(newActivity: string, progessTime: number) {
-    setTimeout(()=> {
-      this.progress = 1.0;
-      this.router.navigate([newActivity]);
-    }, progessTime);
-  };
+  constructor() {}
 
   ngOnInit() {
-    this.addProgress(0.1, 1000, '/home');
-    this.addProgress(0.5, 3000, '/home');
-    this.addProgress(0.8, 5000, '/home');
-    this.addProgress(1.0, 5000, '/home');
   }
 
 }
