7de6fe67494028b274c4bcb874372c5b3dec85a0
John Vincent <corcegajovic@gmail.com>
2021-02-25 16:16:05 +0800

    package: Fully implement Airbnb Lottie

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Lottie/Bodymovin' Support
+import { defineCustomElements as loadLottieBodymovin } from '@teamhive/lottie-player/loader';
+
+loadLottieBodymovin(window);
+
 if (environment.production) {
   enableProdMode();
 }
