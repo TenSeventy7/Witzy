be099d803377a7f7b9d3eb9e63e1a8f2123a4247
John Vincent <corcegajovic@gmail.com>
2021-02-25 16:13:43 +0800

    services/GameStorage: Implement a game storage service
    This service keeps data permanently in the device, so application can get it on demand.

---
+++ src/app/services/game-storage.service.ts
@@ -0,0 +1,21 @@
+import { Plugins } from "@capacitor/core";
+
+const { Storage } = Plugins;
+
+export async function setGameData(key: string, value: any): Promise<void> {
+  await Storage.set({
+    key: key,
+    value: JSON.stringify(value)
+  });
+}
+
+export async function getGameData(key: string): Promise<any> {
+  const item = await Storage.get({ key: key });
+  return JSON.parse(item.value);
+}
+
+export async function deleteGameData(key: string): Promise<void> {
+  await Storage.remove({
+    key: key
+  });
+}
