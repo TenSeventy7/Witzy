2fb5f13c2ae001ab2c6be9bdb6c202136e5cab4f
John Vincent <corcegajovic@gmail.com>
2021-02-25 20:32:27 +0800

    pages/Home: Reflect changes to Lottie
    See 20253a5786b1f9b192eecc61925fa4713c31385f.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { MenuBackgroundComponentModule } from '../components/menu-background/menu-background.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    MenuBackgroundComponentModule,
     RouterModule.forChild([
       {
         path: '',
@@ -18,7 +20,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  declarations: [HomePage]
 })
 export class HomePageModule {}
