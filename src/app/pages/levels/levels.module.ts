99cb04620bfe394807d9837542d6734e75077750
John Vincent <corcegajovic@gmail.com>
2021-02-27 17:21:32 +0800

    witzyRedesign: Do levels screen

--- src/app/pages/levels/levels.module.ts
+++ src/app/pages/levels/levels.module.ts
@@ -5,14 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { LevelsPageRoutingModule } from './levels-routing.module';
-
 import { LevelsPage } from './levels.page';
+
+import { HeaderComponentModule } from '../../components/header/header.module';
+import { MenuBackgroundComponentModule } from '../../components/menu-background/menu-background.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    MenuBackgroundComponentModule,
+    HeaderComponentModule,
     LevelsPageRoutingModule
   ],
   declarations: [LevelsPage]
--- src/app/pages/levels/levels.page.ts
+++ src/app/pages/levels/levels.page.ts
@@ -1,15 +1,95 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Platform } from '@ionic/angular';
+import { GameDataService } from '../../services/game-data.service';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'app-levels',
   templateUrl: './levels.page.html',
   styleUrls: ['./levels.page.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateY(120%)'}),
+        animate('400ms ease', style({transform: 'translateY(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('400ms ease', style({transform: 'translateY(120%)'}))
+      ])
+    ])
+  ],
 })
 export class LevelsPage implements OnInit {
 
-  constructor() { }
+  jsonUrl: string;
+  levels: Array<string> = this.gameData.getGameData('currentLevelData').levels;
+  categoryName: string = this.gameData.getGameData('currentCategoryName');
+  categoryId: string = this.gameData.getGameData('currentCategoryId');
+  modalVisible: boolean = false;
+  inputEnabled: boolean = true;
+  levelNumber: number;
+  selectedLevel: any;
+  levelRemark: string;
+  levelDescription: string;
+  modalFade: string;
+  modalWindowRoll: boolean = false;
+
+  constructor(private router: Router, private platform: Platform, private http: HttpClient, private gameData: GameDataService) { }
 
   ngOnInit() {
+    this.modalVisible = false;
+    this.modalWindowRoll = false;
+  }
+
+  setCurrentLevelData(index) {
+    this.gameData.setGameData('currentLevelId', index.categoryName);
+    this.gameData.setGameData('currentLevelNumber', this.levelNumber);
+
+    this.levelRemark = index.levelRemark;
+    this.levelDescription = index.levelDesc;
+  }
+
+  showLevelModal(index) {
+    this.modalVisible = true;
+    this.inputEnabled = false;
+
+    setTimeout(()=> {
+      this.modalFade = "fadeIn";
+      setTimeout(()=> {
+        this.modalWindowRoll = true;
+        setTimeout(()=> {
+          this.inputEnabled = true;
+          this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+            console.log('Handler was called!');
+            this.onClickOutsideModal();
+          });
+        }, 400);
+      }, 400);
+    }, 300);
+  }
+
+  onClickOutsideModal() {
+    this.modalWindowRoll = false;
+    this.inputEnabled = false;
+
+    setTimeout(()=> {
+      this.modalFade = "fadeOut"
+      setTimeout(()=> {
+        this.modalVisible = false;
+        setTimeout(()=> {
+          this.inputEnabled = true;
+        }, 400);
+      }, 300);
+    }, 400);
+  }
+
+  onClickLevel(levelIndex: number) {
+    this.selectedLevel = this.levels[levelIndex];
+    this.levelNumber = levelIndex + 1;
+    this.setCurrentLevelData(this.selectedLevel);
+    this.showLevelModal(this.selectedLevel);
   }
 
 }
