2609f53c8c0d1b206ea7c973be6431e95675124b
John Vincent <corcegajovic@gmail.com>
2021-02-28 21:39:26 +0800

    pages/Results: Fix errors

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { GameDataService } from '../../services/game-data.service';
 
 @Component({
@@ -11,14 +11,24 @@ export class ResultsPage implements OnInit {
   gameResult: any = 0;
   gameStars: any = 0;
   gameLevel: any = 1;
+  categoryId: any;
   hintText: string;
-  constructor(private route: ActivatedRoute, private scoreData: GameDataService) { }
+  constructor(private router: Router, private scoreData: GameDataService) { }
 
   ngOnInit() {
     this.gameStars = this.scoreData.getGameData('currentGameStars');
     this.gameResult = this.scoreData.getGameData('currentGameScore');
     this.gameLevel = (this.scoreData.getGameData('currentLevelNumberTrue')).toString();
+    this.categoryId = this.scoreData.getGameData('currentCategoryId');
     this.hintText = 'Wow';
+  }
+
+  onClickPlay() {
+    this.router.navigate(['/loading']);
+  }
+
+  onClickBack() {
+    this.router.navigate(['/exit']);
   }
 
 }
