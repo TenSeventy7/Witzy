f587b408126114eae4ddccbd01f6752865a68291
John Vincent <corcegajovic@gmail.com>
2021-03-01 20:55:05 +0800

    components/MenuBackground: Replace Lottie animation with GIF
    Lottie hits performance quite significantly, replace the BG with a GIF.

--- src/app/components/menu-background/menu-background.component.ts
+++ src/app/components/menu-background/menu-background.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AnimationItem } from 'lottie-web';
-import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'witzy-menu-background',
-  template: `
-    <ng-lottie [options]="options" class="background"></ng-lottie>
-  `,
+  templateUrl: './menu-background.component.html',
   styleUrls: ['./menu-background.component.scss'],
 })
 
 export class MenuBackgroundComponent implements CommonModule {
-  options: AnimationOptions = {
-    path: '/assets/bodymovin/space-bg.json',
-  };
 }
--- src/app/components/menu-background/menu-background.module.ts
+++ src/app/components/menu-background/menu-background.module.ts
@@ -1,24 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-import { LottieModule } from 'ngx-lottie';
-import player from 'lottie-web';
- 
-// Note we need a separate function as it's required
-// by the AOT compiler.
-export function playerFactory() {
-  return player;
-}
 
 import { MenuBackgroundComponent } from './menu-background.component';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, 
-    LottieModule.forRoot({ player: playerFactory, 
-      useCache: true, }), IonicModule ],
+  imports: [ CommonModule, FormsModule, IonicModule ],
   declarations: [MenuBackgroundComponent],
   exports: [MenuBackgroundComponent]
 })
