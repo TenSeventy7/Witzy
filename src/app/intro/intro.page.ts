f73cb6bbcb1fb0fbdc32e375c34afa63b131f7d1
John Vincent <corcegajovic@gmail.com>
2021-03-02 17:58:50 +0800

    pages/Intro: Add intro

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  tnsLogo: any;
+  uphLogo: any;
+  textLogo: any;
+
+  constructor(private navCtrl: NavController, private platform: Platform) { }
+
+  ngOnInit() {
+    setTimeout(()=> {
+      this.tnsLogo = "fade"
+      this.uphLogo = ""
+      this.textLogo = ""
+      
+    }, 400);
+
+    setTimeout(()=> {
+      this.tnsLogo = ""
+      this.uphLogo = "fade"
+      this.textLogo = ""
+      
+      setTimeout(()=> {
+        this.tnsLogo = ""
+        this.uphLogo = ""
+        this.textLogo = "fade"
+
+        setTimeout(()=> {
+          this.tnsLogo = ""
+          this.uphLogo = ""
+          this.textLogo = ""
+
+          this.navCtrl.navigateForward(['/splash'], { animated: false })
+          
+        }, 3500);        
+      }, 3500);
+    }, 3500);
+  }
+
+}
