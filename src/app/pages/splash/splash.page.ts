914c9bd8e81710aca00ef92a04ca0b16b24a86c1
John Vincent <corcegajovic@gmail.com>
2021-03-02 18:00:17 +0800

    pages/Splash: Complete

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -1,9 +1,9 @@
-import { TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AudioService } from '../../services/audio.service';
 import { GameDataService } from '../../services/game-data.service';
-import { getGameData, setGameData } from '../../services/game-storage.service';
+import { getGameData } from '../../services/game-storage.service';
 
 @Component({
   selector: 'app-splash',
@@ -20,44 +20,59 @@ export class SplashPage implements OnInit {
   musicEnabled: boolean;
   audioEnabled: boolean;
 
-  constructor(private router: Router, private audio: AudioService, private gameData: GameDataService) { }
+  constructor(private router: Router, private platform: Platform, private audio: AudioService, private gameData: GameDataService) { }
 
-  async ngOnInit() {
+  ngOnInit() {
+    this.splashProgress = 0.0;
+  }
+
+  preloadAudio(key: string, file: string) {
+    this.audio.preload(key, file);
+    this.splashProgress = this.splashProgress + 0.025
+  }
+
+  preloadBgm(key: string, file: string) {
+    this.audio.preloadBgm(key, file);
+    this.splashProgress = this.splashProgress + 0.025
+  }
+
+  async ionViewDidEnter() {
+    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+      console.log("Back disabled.")
+    });
+
     this.splashProgress = 0.1;
 
-    this.preloadAudio('game-bgm-main-menu', '/assets/music/game_bgm_primary.ogg');
-    
+    this.preloadBgm('game-bgm-main-menu', '/assets/music/game_bgm_primary.ogg');    
     this.preloadAudio('game-sfx-correct_1', '/assets/ui/sounds/correct_1.ogg');
     this.preloadAudio('game-sfx-correct_2', '/assets/ui/sounds/correct_2.ogg');
     this.preloadAudio('game-sfx-wrong_1', '/assets/ui/sounds/wrong_1.ogg');
     this.preloadAudio('game-sfx-wrong_2', '/assets/ui/sounds/wrong_2.ogg');
     this.preloadAudio('game-sfx-alert', '/assets/ui/sounds/alert.ogg');
     this.preloadAudio('game-sfx-back', '/assets/ui/sounds/back.ogg');
+    this.preloadAudio('game-sfx-results', '/assets/ui/sounds/results.ogg');
     this.preloadAudio('game-sfx-click', '/assets/ui/sounds/click.ogg');
+    this.preloadAudio('game-sfx-start', '/assets/ui/sounds/start.ogg');
     this.preloadAudio('game-sfx-confirm', '/assets/ui/sounds/confirm.ogg');
     this.preloadAudio('game-sfx-drop', '/assets/ui/sounds/drop.ogg');
     this.preloadAudio('game-sfx-select', '/assets/ui/sounds/select.ogg');
     this.preloadAudio('game-sfx-toggle', '/assets/ui/sounds/toggle.ogg');
 
-    await this.gameData.getGameInfo('/assets/categories/categories.json', 'categoryData');
-    this.categoryData = this.gameData.getGameData('categoryData').categories;
+    await this.gameData.getGameInfo('/assets/categories/categories.json', 'categoryData', true);
+    this.receivedcategoryData =  await this.gameData.getPersistentGameData('categoryData');
+    
+    this.categoryData =  this.receivedcategoryData.categories;
 
-    this.musicEnabled = await getGameData("game_music");
-    this.audioEnabled = await getGameData("game_audio");
-
-    if (this.musicEnabled == null) {
+    if (await getGameData("game_music") == null || await getGameData("game_audio") == null ) {
       this.audio.setBgmState(true);
-    }
-
-    if (this.audioEnabled == null) {
       this.audio.setSfxState(true);
     }
     
     for (var index = 0; index < this.categoryData.length; index++) {
       this.categoryId = this.categoryData[index].categoryId;
-      await this.gameData.getGameInfo(this.categoryData[index].jsonUrl, "currentLevelData_"+this.categoryId);
+      await this.gameData.getGameInfo(this.categoryData[index].jsonUrl, "currentLevelData_"+this.categoryId, true);
       this.completeLevelData = await this.gameData.getUnlockedLevels(this.categoryId);
-      this.gameData.setGameData("currentLevelData_"+this.categoryId, this.completeLevelData);
+      this.gameData.setPersistentGameData("currentLevelData_"+this.categoryId, this.completeLevelData);
       this.splashProgress = this.splashProgress + 0.05
     }
 
@@ -68,8 +83,7 @@ export class SplashPage implements OnInit {
     }, 1000);
   }
 
-  preloadAudio(key: string, file: string) {
-    this.audio.preload(key, file);
-    this.splashProgress = this.splashProgress + 0.025
+  ionViewDidLeave() {
+    this.splashProgress = 0.0;
   }
 }
