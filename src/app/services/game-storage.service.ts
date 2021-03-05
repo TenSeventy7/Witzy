a236d7d357d8ce348730df281b11b35801478bc4
John Vincent <corcegajovic@gmail.com>
2021-03-05 20:55:14 +0800

    services/GameStorage: Only encode IDs

--- src/app/services/game-storage.service.ts
+++ src/app/services/game-storage.service.ts
@@ -3,18 +3,16 @@ const { Storage } = Plugins;
 
 export async function setGameData(key: string, value: any): Promise<void> {
   const encodedKey = btoa(key);
-  const encodedData = btoa(JSON.stringify(value));
   await Storage.set({
     key: encodedKey,
-    value: JSON.stringify(encodedData)
+    value: JSON.stringify(value)
   });
 }
 
 export async function getGameData(key: string): Promise<any> {
   const encodedKey = btoa(key);
   const item = await Storage.get({ key: encodedKey });
-  const decodedData = atob(JSON.parse(item.value));
-  return JSON.parse(decodedData);
+  return JSON.parse(item.value);
 }
 
 export async function deleteGameData(key: string): Promise<void> {
